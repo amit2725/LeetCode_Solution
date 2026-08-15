8670808e5f656f3a76971cbb315d84a89d4079fd
amit <105482312+amit2725@users.noreply.github.com>
2026-08-16 01:23:01 +0530

    Time: 0 ms (100%), Space: 30.8 MB (72.15%) - LeetHub

---
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        if(nums.size()==1) return true;
+        int count=0;
+       
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]>nums[i+1]){
+                {
+                 if(i!=0 && nums[i-1]>nums[i+1]){
+                    nums[i+1]=nums[i];
+                  count++;
+                  }
+                  else count++;
+                }
+                
+            }
+            //else if(temp>nums[i]) count++;
+           
+            if(count>1) return false;
+        }
+       
+        return true;
+    }
+};
