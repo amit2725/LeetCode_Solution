7f50ae9caef787cb1ecd97af18951ec295394fe2
amit <105482312+amit2725@users.noreply.github.com>
2026-08-30 23:07:01 +0530

    Time: 0 ms (100%), Space: 32.6 MB (6.35%) - LeetHub

--- 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.cpp
+++ 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.cpp
@@ -5,20 +5,21 @@ public:
         vector<int> seen(101, 0);
         int count = 0;
 
-        for(int i = 0; i < nums.size(); i++) {
-            if(seen[nums[i]]==-1) continue;
+        for (int i = 0; i < nums.size(); i++) {
+            if (seen[nums[i]] == -1)
+                continue;
 
-            if(i > 0 && nums[i] != nums[i-1] && seen[nums[i]] !=0 ) {
+            if (i > 0 && nums[i] != nums[i - 1] && seen[nums[i]] != 0) {
 
-                
-                    //count++;
-                    seen[nums[i]]=-1;
-                }
-            
-            else seen[nums[i]]++;
+                seen[nums[i]] = -1;
+            }
+
+            else
+                seen[nums[i]]++;
         }
-        for(int x:seen){
-            if(x>0) count++;
+        for (int x : seen) {
+            if (x > 0)
+                count++;
         }
 
         return count;
