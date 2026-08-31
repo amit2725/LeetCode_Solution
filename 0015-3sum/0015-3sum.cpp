cec6fc8059686705773e6bb3d359905a40fcd45f
amit <105482312+amit2725@users.noreply.github.com>
2026-09-01 01:04:51 +0530

    Time: 43 ms (82.97%), Space: 29.2 MB (46.19%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -6,13 +6,12 @@ public:
 
     vector<vector<int>> ans;
 
-    ranges::sort(nums);
+   ranges::sort(nums);
 
     for (int i = 0; i + 2 < nums.size(); ++i) {
       if (i > 0 && nums[i] == nums[i - 1])
         continue;
-      // Choose nums[i] as the first number in the triplet, then search the
-      // remaining numbers in [i + 1, n - 1].
+    
       int l = i + 1;
       int r = nums.size() - 1;
       while (l < r) {
